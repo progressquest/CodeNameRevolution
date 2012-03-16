92fd74114b80cfacd6ac7ccb4179aa6bdb4338c6
Andrew Mortensen <arangarx@gmail.com>
2012-03-15 22:24:51 -0600

    Just set up a new solution for conversation service.
    Signed-off-by: Andrew Mortensen <arangarx@gmail.com>

---
+++ ConversationService/ConversationService/ConversationService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RevolutionConversationService
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	public class ConversationService : IConversationService
+	{
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+	}
+}
